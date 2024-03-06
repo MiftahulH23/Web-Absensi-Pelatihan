2d1071e01a814e6b7c210d94c548a1261b872db9
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2024-03-06 08:00:14 +0700

    update size logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    <link rel="icon" href="images/logokecil.png" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
@@ -12,14 +13,14 @@
 <body>
     <div>
         <div class="container grid place-items-center mt-16">
-            <div class="w-60 h-20 overflow-hidden">
-                <img src="/images/logobrk.png" alt="logo" class="object-cover">
+            <div class="w-64 h-28 overflow-hidden flex justify-center items-center">
+                <img src="/images/newlogo.png" alt="logo" class="object-cover w-full h-full">
             </div>
-            <div class="relative mt-8 animate-line">
-                <div class="w-full h-2 overflow-hidden mt-24 ml-2">
+            <div class="relative mt-20 animate-line">
+                <div class="w-full h-2 overflow-hidden ml-2">
                     <img src="/images/linered.png" alt="line" class="object-cover">
                 </div>
-                <div class="w-20 h-20 overflow-hidden mt-32 absolute -top-[82px] -left-7">
+                <div class="w-20 h-20 overflow-hidden mt-32 absolute -top-[178px] -left-7">
                     <img src="/images/logokecil.png" alt="line" class="object-cover ">
                 </div>
             </div>
