ede6c60b50f662f5cb24ddd16ea52b019089d232
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2024-03-15 11:34:29 +0700

    merge branch

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,41 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Http\Request;
 
-class RegisterController extends Controller
+class LoginController extends Controller
 {
-    public function showRegistrationForm()
+    public function loginForm()
     {
-        return view('daftar'); // Pastikan Anda sudah memiliki view daftar.blade.php
+        return view('masuk');
     }
 
-    public function register(Request $request)
+    public function login(Request $request)
     {
-        // Validasi data
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:8',
+        // Validation
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
 
-        // Jika validasi gagal
-        if ($validator->fails()) {
-            return redirect()->route('daftar')
-                ->withErrors($validator) // Mengirimkan pesan kesalahan ke tampilan
-                ->withInput(); // Mengirimkan kembali input yang sudah dimasukkan
+        // Check if admin exists
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            // Jika pengguna ditemukan, periksa apakah password cocok
+            if (Hash::check($request->password, $user->password)) {
+                // Authenticated successfully
+                return redirect()->route('beranda');
+            } else {
+                // Password salah, redirect kembali dengan pesan kesalahan
+                return redirect()->back()->with('error', 'Password yang Anda masukkan salah');
+            }
+        } else {
+            // Pengguna tidak ditemukan, redirect kembali dengan pesan kesalahan
+            return redirect()->back()->with('error', 'Email yang Anda masukkan tidak terdaftar');
         }
-
-        // Jika validasi sukses, lanjutkan dengan membuat pengguna baru
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->save();
-
-        return redirect()->route('login'); 
     }
 }
