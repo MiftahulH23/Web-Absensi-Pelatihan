b4ef1b35009abb1ce2035002e583316577b65dd0
None1202 <syarifahghinar@gmail.com>
2024-03-19 14:47:23 +0700

    gambar sudah muncul

--- app/Exports/AbsenExport.php
+++ app/Exports/AbsenExport.php
@@ -2,26 +2,68 @@
 
 namespace App\Exports;
 
-use App\Models\Absen; // Import model Absen
+use App\Models\Absen;
 use Maatwebsite\Excel\Concerns\FromCollection;
-use Illuminate\Support\Collection; // Import Collection
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 
-class AbsenExport implements FromCollection
+class AbsenExport implements FromCollection, WithHeadings, WithDrawings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        $data = [
-            ['No', 'Nama', 'No Rekening', 'Nik', 'Level Jabatan', 'Jabatan', 'Unit Kantor', 'Dokumentasi', 'Tanda Tangan', 'Absen', 'Waktu', 'Status'],
-            // Data selanjutnya
+        return Absen::all();
+    }
+
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        return [
+            'No', 'Nama', 'No Rekening', 'Nik', 'Level Jabatan', 'Jabatan', 'Unit Kantor',
+            'Dokumentasi', 'Tanda Tangan', 'Absen', 'Waktu', 'Status',
         ];
+    }
 
-        // Mengambil semua data dari model Absen
-        $absen = Absen::get()->toArray(); 
+    /**
+     * Menambahkan gambar ke dalam file Excel
+     *
+     * @return array
+     */
+    public function drawings()
+    {
+        $absens = Absen::all();
+        $drawings = [];
 
-        // Gabungkan judul dan data
-        return new Collection(array_merge($data, $absen));
+        foreach ($absens as $index => $absen) {
+            $fotoPath = storage_path('app/public/absens/' . $absen->foto);
+            $ttdPath = storage_path('app/public/ttd/' . $absen->ttd);
+
+            // Membuat objek gambar untuk foto
+            $drawingFoto = new Drawing();
+            $drawingFoto->setName('Foto_' . $index); // Nama unik untuk gambar
+            $drawingFoto->setDescription('Foto');
+            $drawingFoto->setPath($fotoPath);
+            $drawingFoto->setHeight(100); // Sesuaikan dengan tinggi yang diinginkan
+            $drawingFoto->setCoordinates('H' . ($index + 2)); // Sesuaikan dengan kolom yang diinginkan
+
+            // Membuat objek gambar untuk tanda tangan
+            $drawingTtd = new Drawing();
+            $drawingTtd->setName('Tanda_Tangan_' . $index); // Nama unik untuk gambar
+            $drawingTtd->setDescription('Tanda Tangan');
+            $drawingTtd->setPath($ttdPath);
+            $drawingTtd->setHeight(100); // Sesuaikan dengan tinggi yang diinginkan
+            $drawingTtd->setCoordinates('I' . ($index + 2)); // Sesuaikan dengan kolom yang diinginkan
+
+            // Menambahkan objek gambar ke dalam array drawings
+            $drawings[] = $drawingFoto;
+            $drawings[] = $drawingTtd;
+        }
+
+        return $drawings;
     }
 }
