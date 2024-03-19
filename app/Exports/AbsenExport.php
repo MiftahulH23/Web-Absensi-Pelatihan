d7523e819b6e1e2d9e33e41fd3100953ba0284e9
None1202 <syarifahghinar@gmail.com>
2024-03-19 14:38:51 +0700

    update download

--- app/Exports/AbsenExport.php
+++ app/Exports/AbsenExport.php
@@ -4,6 +4,7 @@ namespace App\Exports;
 
 use App\Models\Absen; // Import model Absen
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Support\Collection; // Import Collection
 
 class AbsenExport implements FromCollection
 {
@@ -12,7 +13,15 @@ class AbsenExport implements FromCollection
     */
     public function collection()
     {
+        $data = [
+            ['No', 'Nama', 'No Rekening', 'Nik', 'Level Jabatan', 'Jabatan', 'Unit Kantor', 'Dokumentasi', 'Tanda Tangan', 'Absen', 'Waktu', 'Status'],
+            // Data selanjutnya
+        ];
+
         // Mengambil semua data dari model Absen
-        return Absen::get(); 
+        $absen = Absen::get()->toArray(); 
+
+        // Gabungkan judul dan data
+        return new Collection(array_merge($data, $absen));
     }
 }
